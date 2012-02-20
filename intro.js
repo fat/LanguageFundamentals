d90051d99519a28de3c90e8a6980de827934208a
Jacob Thornton <jacobthornton@gmail.com>
2012-02-19 16:48:11 -0800

    typo in language

--- intro.js
+++ intro.js
@@ -11,7 +11,7 @@
 // |_|  \__,_|_| |_|\__,_|\__,_|_| |_| |_|\___|_| |_|\__\__,_|_|___/
 //
 //
-// An introduction to the lanugage of JavaScript by way of vague
+// An introduction to the language of JavaScript by way of vague
 // recollections and hasty (not thorough or detailed) failing unit tests.
 //
 // <3 @fat, a class taught in 2012
