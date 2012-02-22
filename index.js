e964acc0fd835aad4fc8dd57ad75680282614577
Jacob Thornton <jacobthornton@gmail.com>
2012-02-21 20:06:49 -0800

    add notes

--- index.js
+++ index.js
@@ -52,13 +52,66 @@
 // JavaScript's standard global objects and how to work with them.
 //
 // Array
+
+var fat = ['dave', 'is', 'here'];
+
+var mdo = new Array('mark', 'is', 'not');
+
 // Boolean
+
+var blah = true;
+blah = false;
+
 // Date
+
+new Date('');
+
 // Function
+
+function patrick (){
+
+}
+
+var patrick = function () {
+
+}
+
 // Number
+
+234
+
 // Object
+
+new Object
+
+var jacob = 'blah'
+
+var obj = {
+	'blah': 'value',
+	'shail': true
+}
+
+
+$('div').addClass('foo');
+
+$.prototype.myPlugin = function () {
+
+}
+
+
 // RegExp
+
+var foo = new RegExp('blah', 'gi')
+
+var foo = /blah/gi
+
+
 // String
+
+var foo = "fat"
+
+foo.split('a') // ['f', 't']
+
 //
 //
 //
@@ -79,10 +132,27 @@
 //
 // falsey
 // undefined, null, NaN, 0, "" (empty string), and false
+
+var x = $('div')
+
+	if (x.length === 0) {
+		console.log('hi')
+	}
+
+	if (x.length) {
+		console.log('hi)')
+	}
+
 //
 // truthy
 // 'o_o'
 //
+
+
+	if ('o_o') {
+		console.log('hi')
+	}
+
 //
 //
 //
@@ -98,21 +168,132 @@
 // JavaScript's keywords and how to use them
 //
 // if/else
+
+var x;
+
+if (true) {
+	x = 'bla';
+} else {
+	x = 'blah';
+}
+
 // switch/case/default
+
+var x = 0;
+
+switch (x) {
+	case 0:
+	  x = 0 + 1;
+	  break;
+	case typeof x == 'number':
+	  x = 'blah'
+	  break;
+	default:
+	  x = 'patrick'
+}
+
 // for
+
+var fat = ['is', 'not', 'fat'];
+
+var x = 0;
+
+for (var i = 0; i < fat.length; i++) {
+	console.log(fat[i]);
+}
+
+
 // in
+
+var x = { bad: 'blah' }
+
+for (var i in x) {
+
+}
+
 // while/do
+
+
+while (true)  {
+
+}
+
+do {
+
+} while (true)
+
 // break/continue
+
+for (var i = 0; i < fat.length; i++) {
+	if ( i === 4) break;
+}
+
+
 // throw/try/catch/finally
-// return
+
+try {
+
+  throw 'twitter is cool.'
+
+  throw new Error('marcus is a smarty pants.')
+
+} catch (e) {
+
+} finally {
+
+}
+
 // function
+
+ function () {
+
+ }
+
+// return
+
+  function asdf() {
+  	 return 'asdf'
+  }
+
 // var
+
+var asdf, asdf1, asfd2;
+
+	two
+  , asdf = 'blah'
+  , mail = 'twwer';
+
+
+
 // delete
+
+
+
+ delete object.property
+
 // with
+
+with (Math) {
+	var x = pi + 2
+}
+
+Math.pi + 2 = sdf
+
 // typeof
+
+typeof 'foo' == 'string'
+
+
 // this
+
+
 // new
+
+
+
 // instanceof
+
+// debugger
 //
 //
 //
@@ -148,12 +329,63 @@
 // JS Functions, Closures, and Blocks
 //
 // function arguments
+
+ function on () {
+   arguments[0] == 1
+   foo == 1
+ }
+
+$('div').on('click', function () {
+
+})
+
+$('div').on('click', 'a', function () {
+
+})
+
+jacob (1,2,3,4,4,5)
+
 // function expressions
+
+var DAVE = function (str, array, fn) {
+
+}
+
 // recursion
+
 // scope
+
+
+function foo() {
+
+	var y;
+
+	console.log(x);
+
+	function bar() {
+
+	}
+
+}
+
+var x = '';
+
+alert(y) // ;_;
+
 // closures
 // blocks
-//
+
+
+if () {
+	var jacob = true;
+}
+
+for () {
+
+}
+
+
+
 //
 //
 //
